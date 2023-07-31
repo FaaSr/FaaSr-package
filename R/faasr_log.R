2b308a3ac2b4525682420ceec8ed5c4ad90894c7
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-07-31 18:49:49 -0400

    Update faasr_log.R

--- R/faasr_log.R
+++ R/faasr_log.R
@@ -31,7 +31,7 @@ faasr_log <- function(faasr,log_message) {
 		  secret_access_key=log_server$SecretKey
 		)
 	  ),
-	  endpoint=log_server$Endpoint
+	  endpoint=log_server$Endpoint,
 	  region=log_server$Region
 	)
   )
