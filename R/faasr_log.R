587adabc9dc2f256d98aaa9e78c3654d850e8f4e
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-09-02 01:53:35 -0400

    Update faasr_log.R

--- R/faasr_log.R
+++ R/faasr_log.R
@@ -7,13 +7,13 @@
 
 library("paws")
 
-faasr_log <- function(faasr,log_message) {
+faasr_log <- function(log_message) {
 
   # Extract name of logging server
-  log_server_name = faasr$LoggingServer
+  log_server_name = .faasr$LoggingServer
 
   # Validate that server_name exists, otherwise abort
-  if (log_server_name %in% names(faasr$DataStores)) {
+  if (log_server_name %in% names(.faasr$DataStores)) {
     NULL
    } else {
      err_msg <- paste0('{\"faasr_log\":\"Invalid logging server name: ',log_server_name,'\"}', "\n")
@@ -21,7 +21,7 @@ faasr_log <- function(faasr,log_message) {
      stop()
    }
 
-  log_server <- faasr$DataStores[[log_server_name]]
+  log_server <- .faasr$DataStores[[log_server_name]]
 
   s3<-paws::s3(
     config=list(
@@ -37,8 +37,8 @@ faasr_log <- function(faasr,log_message) {
   )
 
   # set file name to be "faasr_log_" + faasr$InvocationID + faasr$FunctionInvoke + ".txt"
-  log_folder <- paste0(faasr$FaaSrLog, "/", faasr$InvocationID)
-  log_file <- paste0(log_folder, "/", faasr$FunctionInvoke,".txt")
+  log_folder <- paste0(.faasr$FaaSrLog, "/", .faasr$InvocationID)
+  log_file <- paste0(log_folder, "/", .faasr$FunctionInvoke,".txt")
   if (!dir.exists(log_folder)){dir.create(log_folder, recursive=TRUE)}
 
   check_log_file <- s3$list_objects_v2(Bucket=log_server$Bucket, Prefix=log_file)
