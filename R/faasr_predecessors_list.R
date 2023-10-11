7b221ececd30a78a0efb99dbe0d091b743ac20a0
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-10-10 21:32:51 -0400

    Update faasr_predecessors_list.R

--- R/faasr_predecessors_list.R
+++ R/faasr_predecessors_list.R
@@ -6,10 +6,10 @@
 faasr_predecessors_list <- function(faasr, graph){
   # find the predecessors and add them to the list "pre"
   pre <- list()
-  for (func in names(faasr$FunctionList)) {
-    if (faasr$FunctionInvoke %in% graph[[func]]) {
-	  pre <- c(pre, func)
+  for (func1 in names(graph)){
+		for (func2 in graph[[func1]]){
+			pre[[func2]] <- c(pre[[func2]], func1)
+		}
 	}
-  }
   return(pre)
 }
