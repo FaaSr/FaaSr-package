8c45d8f10fd654a3c102b3c759f36a2d40d63a59
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2023-11-25 15:03:53 -0500

    Update faasr_log.R

--- R/faasr_log.R
+++ R/faasr_log.R
@@ -8,8 +8,17 @@
 library("paws")
 
 faasr_log <- function(log_message) {
+  # set file name to be "faasr_log_" + faasr$InvocationID + faasr$FunctionInvoke + "_local.txt"
+  log_folder <- paste0(.faasr$FaaSrLog, "/", .faasr$InvocationID)
+  log_file_local <- paste0(log_folder, "/", .faasr$FunctionInvoke,"_local.txt")
+  if (!dir.exists(log_folder)){dir.create(log_folder, recursive=TRUE)}
 
-  # Extract name of logging server
+  # only write the log for the local file.
+  logs <- log_message
+  write.table(logs, log_file_local, col.names=FALSE, row.names = FALSE, append=TRUE, quote=FALSE)
+}
+
+faasr_log_flush <- function(){
   if (is.null(.faasr$LoggingDataStore)){
     log_server_name = .faasr$DefaultDataStore
   } else {
@@ -18,42 +27,47 @@ faasr_log <- function(log_message) {
   # Validate that server_name exists, otherwise abort
   if (log_server_name %in% names(.faasr$DataStores)) {
     NULL
-   } else {
-     err_msg <- paste0('{\"faasr_log\":\"Invalid logging server name: ',log_server_name,'\"}', "\n")
-     cat(err_msg)
-     stop()
-   }
-
+  } else {
+    err_msg <- paste0('{\"faasr_log\":\"Invalid logging server name: ',log_server_name,'\"}', "\n")
+    cat(err_msg)
+    stop()
+  }
+  
   log_server <- .faasr$DataStores[[log_server_name]]
-
+  
   s3<-paws::s3(
     config=list(
-	  credentials=list(
-	    creds=list(
-		  access_key_id=log_server$AccessKey,
-		  secret_access_key=log_server$SecretKey
-		)
-	  ),
-	  endpoint=log_server$Endpoint,
-	  region=log_server$Region
-	)
+      credentials=list(
+        creds=list(
+          access_key_id=log_server$AccessKey,
+          secret_access_key=log_server$SecretKey
+        )
+      ),
+      endpoint=log_server$Endpoint,
+      region=log_server$Region
+    )
   )
-
+  
   # set file name to be "faasr_log_" + faasr$InvocationID + faasr$FunctionInvoke + ".txt"
   log_folder <- paste0(.faasr$FaaSrLog, "/", .faasr$InvocationID)
   log_file <- paste0(log_folder, "/", .faasr$FunctionInvoke,".txt")
-  if (!dir.exists(log_folder)){dir.create(log_folder, recursive=TRUE)}
-
-  check_log_file <- s3$list_objects_v2(Bucket=log_server$Bucket, Prefix=log_file)
-  if (length(check_log_file$Contents) != 0) {
-	  if (file.exists(log_file)) {
-	    file.remove(log_file)
-	  }
-	  s3$download_file(Bucket=log_server$Bucket, Key=log_file, Filename=log_file)
+  log_file_local <- paste0(log_folder, "/", .faasr$FunctionInvoke,"_local.txt")
+  if (file.exists(log_file_local)){
+    logs_local <- read.table(log_file_local)
+    
+    if (!dir.exists(log_folder)){dir.create(log_folder, recursive=TRUE)}
+    
+    check_log_file <- s3$list_objects_v2(Bucket=log_server$Bucket, Prefix=log_file)
+    if (length(check_log_file$Contents) != 0) {
+      if (file.exists(log_file)) {
+        file.remove(log_file)
+      }
+      s3$download_file(Bucket=log_server$Bucket, Key=log_file, Filename=log_file)
+    }
+     # append message to the local file then upload
+    write.table(logs_local, log_file, col.names=FALSE, row.names = FALSE, append=TRUE, quote=FALSE)
+    s3$put_object(Body=log_file, Key=log_file, Bucket=log_server$Bucket)
   }
-
-  # append message to the local file then upload
-  logs <- log_message
-  write.table(logs, log_file, col.names=FALSE, row.names = FALSE, append=TRUE, quote=FALSE)
-  s3$put_object(Body=log_file, Key=log_file, Bucket=log_server$Bucket)
+  
 }
+
