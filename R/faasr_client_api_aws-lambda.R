bc297382a8f595cac1b8131b86c6b8dfb6fad4e8
Sungjae Park <123699102+spark0510@users.noreply.github.com>
2024-02-08 23:08:36 -0500

    Update faasr_client_api_aws-lambda.R

--- R/faasr_client_api_aws-lambda.R
+++ R/faasr_client_api_aws-lambda.R
@@ -150,7 +150,7 @@ faasr_register_workflow_lambda_function_lists <- function(faasr,cred, lambda_ser
     current_lambda_server_info <- lambda_server_info[[server_name]]
 
     if(check_lambda_exists(action_name, cred, current_lambda_server_info)){
-      cli_alert_info(paste0("lambda function - {.strong", action_name, "} already exists."))
+      cli_alert_info(paste0("lambda function - {.strong ", action_name, "} already exists."))
       cli_alert_info("Do you want to update?")
       while(TRUE) {
         check <- readline()
