0c4093717228505c84669f61443f9bfd7467037b
gokulkeshav <gokulkeshavosu@gmail.com>
2025-02-03 00:41:09 -0800

    Add trigger for Google Cloud

--- R/faasr_trigger.R
+++ R/faasr_trigger.R
@@ -70,6 +70,47 @@ faasr_trigger <- function(faasr) {
         next_server_type <- faasr$ComputeServers[[next_server]]$FaaSType
 
         switch(next_server_type,
+          "GoogleCloud"={
+            #
+            # GoogleCloud API handling
+            #
+            # Set the env values for the google cloud action.
+            # GoogleCloud API handling
+            endpoint <- faasr$ComputeServers[[next_server]]$Endpoint
+            token <- faasr$ComputeServers[[next_server]]$API.key
+
+            if (is.null(faasr$ComputeServers[[next_server]]$SSL) || faasr$ComputeServers[[next_server]]$SSL == "") {
+              ssl <- TRUE
+            } else {
+              ssl <- as.logical(toupper(faasr$ComputeServers[[next_server]]$SSL))
+            }
+
+            headers <- c(
+              'accept' = 'application/json',
+              'Content-Type' = 'application/json',
+              'Authorization' = paste("Bearer", token)  # Set Authorization header directly
+            )
+
+            # Send the REST request (POST/GET/PUT/PATCH)
+            response <- POST(
+              url = endpoint,
+              add_headers(.headers = headers),
+              body = faasr,
+              encode = "json",
+              httr::config(ssl_verifypeer = ssl, ssl_verifyhost = ssl),
+              accept_json()
+            )
+
+            if (response$status_code == 200 || response$status_code == 202) {
+              succ_msg <- paste0('{\"faasr_trigger\":\"GoogleCloud: Successfully invoked: ', faasr$FunctionInvoke, '\"}\n')
+              message(succ_msg)
+              #faasr_log(succ_msg) # TODO: Add this back in
+            } else {
+              err_msg <- paste0('{\"faasr_trigger\":\"GoogleCloud: Error invoking: ', faasr$FunctionInvoke, ' - ', content(response)$error, '\"}\n')
+              message(err_msg)
+              #faasr_log(err_msg) # TODO: Add this back in
+            }
+          },
           # if OpenWhisk - use OpenWhisk API
           "OpenWhisk"={
             #
